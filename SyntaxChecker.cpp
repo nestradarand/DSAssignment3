3ecb3700657463e58e03401ea79b746f2fe2f202
nestradarand <noah.estrada456@gmail.com>
2019-10-10 23:49:41 +0000

    created basic class files

---
+++ SyntaxChecker.cpp
@@ -0,0 +1,15 @@
+#include "SyntaxChecker.h"
+#include <iostream>
+#include <string>
+#include "GenStack.h"
+
+using namespace std;
+
+SyntaxChecker::SyntaxChecker()
+{
+
+}
+SyntaxChecker::~SyntaxChecker()
+{
+
+}
---
+++ SyntaxChecker.h
@@ -0,0 +1,12 @@
+#ifndef SYNTAXCHECKER_H
+#define SYNTAXCHECKER_H
+#include "GenStack.h"
+
+class SyntaxChecker
+{
+public:
+  SyntaxChecker();
+  ~SyntaxChecker();
+
+};
+#endif
